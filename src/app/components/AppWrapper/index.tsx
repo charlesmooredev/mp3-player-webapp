eabfcf704e920575eb1863e69264d759c1f9ff22
Charles Moore <charles@ozone.tech>
2024-08-09 19:38:31 -0400

    Fix of the overflow

--- src/app/components/AppWrapper/index.tsx
+++ src/app/components/AppWrapper/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function AppWrapper({ children }: Props) {
   return (
-    <div className="flex h-screen w-screen justify-center overflow-hidden bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900">
+    <div className="flex h-screen w-screen justify-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900">
       <div className="flex w-full flex-col items-center space-y-1 px-2 lg:w-[1280px] lg:px-0">{children}</div>
     </div>
   );
